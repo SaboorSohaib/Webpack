a46252c5b6c60df22b2b607b16c5cf3240a36181
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-04 17:58:39 +0430

    Add webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = {
+    mode: 'development',
+  };
